0ba8024be3aceb47294e0cdd072cdb03535f9669
Toxic254 <cyrus_g@outlook.com>
2021-09-21 23:01:12 +0300

    Created RegisterUser Serializer and LoginSerializer and EmailSerializer.

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -2,17 +2,21 @@
 from rest_framework import serializers
 
 # DRF Simple JWT.
-# from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Custom Models.
 from .models import User
+
+# Utils.
+from django.contrib.auth import authenticate
+from rest_framework.exceptions import AuthenticationFailed
 
 # Input Validator
 from rest_framework.validators import UniqueTogetherValidator
 
 # Serializers.
 
-class UserSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(min_length=6, write_only=True)
     isSuperUser = serializers.SerializerMethodField(read_only=True)
     isAdmin = serializers.SerializerMethodField(read_only=True)
@@ -23,7 +27,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'email', 'username', 'isAdmin', 'isSuperUser','password']
+        fields = ['id', 'email', 'username', 'isAdmin', 'isSuperUser', 'password']
 
     def validate(self, obj):
         email = obj.get('email', '')
@@ -47,8 +51,55 @@ class UserSerializer(serializers.ModelSerializer):
 class EmailVerificationSerializer(serializers.ModelSerializer):
     token = serializers.CharField(max_length=555)
 
-
     class Meta:
         model = User
         fields = ['token']
         
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=3)
+    password = serializers.CharField(
+        max_length=68, min_length=6, write_only=True)
+    username = serializers.CharField(
+        max_length=255, min_length=3, read_only=True)
+
+    tokens = serializers.SerializerMethodField()
+
+    def get_tokens(self, obj):
+        user = User.objects.get(email=obj['email'])
+
+        return {
+            'refresh': user.tokens()['refresh'],
+            'access': user.tokens()['access']
+        }
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'username', 'tokens']
+
+    def validate(self, obj):
+        email = obj.get('email', '')
+        password = obj.get('password', '')
+        # filtered_user_by_email = User.objects.filter(email=email)
+        user = authenticate(email=email, password=password)
+
+        # if filtered_user_by_email.exists() and filtered_user_by_email[0].auth_provider != 'email':
+        #     raise AuthenticationFailed(
+        #         detail='Please continue your login using ' + filtered_user_by_email[0].auth_provider)
+
+        if not user:
+            raise AuthenticationFailed('Invalid credentials, try again')
+        
+        if not user.is_active:
+            raise AuthenticationFailed('Account disabled, contact admin')
+        
+        if not user.is_verified:
+            raise AuthenticationFailed('Email is not verified')
+            
+        return {
+            'email': user.email,
+            'username': user.username,
+            'tokens': user.tokens
+        }
+
+        return super().validate(attrs)
+
