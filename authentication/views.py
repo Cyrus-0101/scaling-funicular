9ee601d03a2f94a210a36b8f8823bf7186937090
Toxic254 <cyrus_g@outlook.com>
2021-09-21 23:03:40 +0300

    Rewritten Register, Email Verification and  LoginAPI view.

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,11 @@
+import os
+
 # Generic API View, HTTP Status Codes, Views.
 from rest_framework import generics, status, views
 
 # JWT
 import jwt
+
 from django.conf import settings
 
 # il8n
@@ -12,7 +15,7 @@ from django.utils.translation import gettext as _
 from rest_framework.response import Response
 
 # Serializers.
-from .serializers import EmailVerificationSerializer, UserSerializer
+from .serializers import EmailVerificationSerializer, RegisterSerializer, LoginSerializer
 
 # Tokens.
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -36,73 +39,174 @@ from drf_yasg import openapi
 # Create your views here.
 class RegisterView(generics.GenericAPIView):
 
-    serializer_class = UserSerializer
+    serializer_class = RegisterSerializer
 
     def post(self, request):
         user = request.data
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
         user_data = serializer.data
         user = User.objects.get(email=user_data['email'])
-
-        token = str(RefreshToken.for_user(user).access_token)
-
+        token = RefreshToken.for_user(user).access_token
         current_site = get_current_site(request).domain
-
         relativeLink = reverse('email-verify')
-
-        absurl = f'http://{current_site}{relativeLink}?token={token}'
-        print(absurl)
-        email_body = f'Hi {user.username}.\nWelcome To Mbuzi Munch Loyalty Program.\nUse the link below to verify your email: \n{absurl}'
-        data = {
-            'email-subject': 'Verify your Email Address.',
-            'email-body': email_body,
-            'to-email': user.email 
-        }
+        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+        email_body = 'Hi '+user.username + \
+            ' Use the link below to verify your email \n' + absurl
+        data = {'email_body': email_body, 'to_email': user.email,
+                'email_subject': 'Verify your email'}
 
         Util.send_email(data)
-
         return Response(user_data, status=status.HTTP_201_CREATED)
 
+
 class VerifyEmail(views.APIView):
-    
     serializer_class = EmailVerificationSerializer
 
     token_param_config = openapi.Parameter(
-        'token',
-        in_=openapi.IN_QUERY,
-        description="Enter token to validate email.",
-        type=openapi.TYPE_STRING
-    )
+        'token', in_=openapi.IN_QUERY, description='Description', type=openapi.TYPE_STRING)
 
-    
     @swagger_auto_schema(manual_parameters=[token_param_config])
     def get(self, request):
         token = request.GET.get('token')
-        
         try:
             payload = jwt.decode(token, settings.SECRET_KEY)
             user = User.objects.get(id=payload['user_id'])
             
-            if not user.is_verified:
+            if not user.is_verified or user.is_active:
                 user.is_verified = True
+                user.is_active = True
 
                 user.save()
 
-            return Response(
-                {'email': 'Successfully Activated Email Address.'},
-                status=status.HTTP_200_OK
-            )
+            return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
         except jwt.ExpiredSignatureError as identifier:
-            return Response(
-                {'error': 'Token  Expired.'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
+            return Response({'error': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
         except jwt.exceptions.DecodeError as identifier:
-            return Response(
-                {'error': 'Invalid Token.'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+            return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class RegisterView(generics.GenericAPIView):
+
+#     serializer_class = RegisterSerializer
+
+#     def post(self, request):
+#         user = request.data
+#         serializer = self.serializer_class(data=user)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+
+#         user_data = serializer.data
+#         user = User.objects.get(email=user_data['email'])
+
+#         token = RefreshToken.for_user(user).access_token
+
+#         current_site = get_current_site(request).domain
+
+#         relativeLink = reverse('email-verify')
+
+#         absurl = 'http://'+current_site+relativeLink+'?token='+str(token)
+
+#         email_body = f'Hi {user.username}.\nWelcome To Mbuzi Munch Loyalty Program.\nUse the link below to verify your email: \n{absurl}'
+#         data = {
+#             'email-subject': 'Verify your Email Address.',
+#             'email-body': email_body,
+#             'to-email': user.email 
+#         }
+
+#         Util.send_email(data)
+
+#         return Response(user_data, status=status.HTTP_201_CREATED)
+
+# class VerifyEmail(views.APIView):
+    
+#     serializer_class = EmailVerificationSerializer
+
+#     token_param_config = openapi.Parameter(
+#         'Token',
+#         in_=openapi.IN_QUERY,
+#         description="Enter token to validate email.",
+#         type=openapi.TYPE_STRING
+#     )
+
+    
+#     @swagger_auto_schema(manual_parameters=[token_param_config])
+#     def get(self, request):
+#         token = request.GET.get('token')
+        
+#         try:
+#             payload = jwt.decode(token, os.environ.get('SECRET_KEY'), algorithms="HS256")
+            
+#             user = User.objects.get(id=payload['user_id'])
+            
+#             if not user.is_verified:
+#                 user.is_verified = True
+
+#                 user.save()
+
+#             email_body = f'Hi {user.username}.\nWelcome To Mbuzi Munch Loyalty Program. You have successfully activated your account.\nCheckout our App and be able to order in-house. Thank you for choosing Mbuzi :): \n'
+#             data = {
+#                 'email-subject': 'Welcome! Successfully Activated Account.',
+#                 'email-body': email_body,
+#                 'to-email': user.email 
+#             }
+
+#             Util.send_email(data)
+
+
+#             return Response(
+#                 {'detail': 'Successfully Activated Email Address.'},
+#                 status=status.HTTP_200_OK
+#             )
+#         except jwt.ExpiredSignatureError as identifier:
+#             return Response(
+#                 {'error': 'Token  Expired.'},
+#                 status=status.HTTP_400_BAD_REQUEST
+#             )
+
+#         except jwt.exceptions.DecodeError as identifier:
+#             return Response(
+#                 {'error': 'Invalid Token.'},
+#                 status=status.HTTP_400_BAD_REQUEST
+#             )
+
+# class LoginView(generics.GenericAPIView):
+    
+#     serializer_class = LoginSerializer
+
+#     def post(self, request):
+#         serializer = self.serializer_class(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
