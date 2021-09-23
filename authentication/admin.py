a1391bcc91cc31c463facedc2e573fd0c69b9374
Toxic254 <cyrus_g@outlook.com>
2021-09-23 14:29:41 +0300

    Configured how we want our admin site should look for our User Object.

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,53 @@
-from authentication.models import User
+# # Django Modules.
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+# User Model
+from authentication.models import User
+
+# Admin Page Configuration.
+class UserAdminConfig(UserAdmin):
+    model = User
+    search_fields = [
+        'email',
+        'username',
+        'is_active',
+        'is_staff',
+        'is_verified'
+    ]
+    list_filter = [
+        'email',
+        'username',
+    ]
+    ordering = ['date_joined']
+    list_display = [
+        'email',
+        'username',
+        'is_active',
+        'is_staff',
+        'date_joined'
+    ]
+
+    fieldsets = [
+        ('User Details', {'fields': ('email', 'username', 'is_verified')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser',)}),
+        ('Personal', {'fields': ('date_joined', 'birthday', 'last_login',)})
+    ]
+
+    add_fieldsets = [
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'username',
+                'password1',
+                'password2',
+                'is_active',
+                'is_staff',
+            )
+        })
+    ]
+
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdminConfig)
