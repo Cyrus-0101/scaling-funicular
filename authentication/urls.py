f9d91b73db4b26497c708b3c92d61a242f90a572
Toxic254 <cyrus_g@outlook.com>
2021-09-24 14:20:08 +0300

    Added admin and user routes.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
-from .views import LoginAPIView, RegisterView, VerifyEmail
+from .views import DeleteUser, LoginAPIView, RegisterView, UpdateUserInformation, VerifyEmail, adminGetUserById, getUserProfile, getUsers
+
+# /auth/
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name="register-user"),
     path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
     path('login/', LoginAPIView.as_view(), name='login-user'),
 
-    # path('profile/', views.get_user_profile, name="user-profile"),
-    # path('profile/update/', views.update_user_profile, name="update-user-profile"),
+    path('profile/', getUserProfile, name="user-profile"),
+    path('profile/update/', UpdateUserInformation.as_view(), name="update-user-profile"),
 
-    # path('', views.admin_get_users, name="all-users"),
-    # path('<str:pk>/', views.admin_get_user_by_id, name="delete-user"),
-    # path('delete/<str:pk>/', views.admin_delete_user, name="delete-user"),
-    # path('update/<str:pk>/', views.admin_update_user_profile, name="delete-user"),
+    path('admin-users/', getUsers, name="admin-all-users"),
+    path('admin-users/<str:pk>/', adminGetUserById, name="admin-get-user-by-id"),
+    path('admin/delete/<str:pk>/', DeleteUser.as_view(), name="admin-delete-user"),
+    path('admin/update/<str:pk>/', UpdateUserInformation.as_view(), name="admin-update-user"),
     
 
 ]
