2bf3017463ff8e5c213e1d4aac5c5aaeede7716e
Toxic254 <cyrus_g@outlook.com>
2021-09-28 11:42:49 +0300

    Added new URL Routes, RequestPasswordReset, SetNewUserPassword, and TokenRefresh.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
-from .views import DeleteUser, LoginAPIView, RegisterView, UpdateUserInformation, VerifyEmail, adminGetUserById, getUserProfile, getUsers
+
+# Views.
+from .views import DeleteUser, LoginAPIView, PasswordTokenCheckAPI, RegisterView, RequestPasswordReset, SetNewUserPassword, UpdateUserInformation, VerifyEmail, adminGetUserById, getUserProfile, getUsers
+
+# Rest Framework TokenRefresh.
+from rest_framework_simplejwt.views import TokenRefreshView
 
 # /auth/
 
 urlpatterns = [
+
     path('register/', RegisterView.as_view(), name="register-user"),
     path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
     path('login/', LoginAPIView.as_view(), name='login-user'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('password-reset/<uidb64>/<token>/', PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+    path('password-reset-email/', RequestPasswordReset.as_view(), name='password-reset-email'),
+    path('password-reset-complete/', SetNewUserPassword.as_view(), name='password-reset-complete'),
 
     path('profile/', getUserProfile, name="user-profile"),
     path('profile/update/', UpdateUserInformation.as_view(), name="update-user-profile"),
